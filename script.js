ab25dd07361a1bf39c4b3ff22a69550468b175f7
Om Pathak <85027954+Gamferno@users.noreply.github.com>
2024-09-08 17:02:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var y = Math.floor((Math.random() * 6) + 1);
 console.log(x);
 console.log(y);
 
-document.getElementsByClassName("img1")[0].src = "/images/dice"+x+".png";
+document.getElementsByClassName("img1")[0].src = "images/dice"+x+".png";
 
-document.getElementsByClassName("img2")[0].src = "/images/dice"+y+".png";
+document.getElementsByClassName("img2")[0].src = "images/dice"+y+".png";
 if(x > y){
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 }else if(x <y){
