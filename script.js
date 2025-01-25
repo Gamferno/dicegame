6191e252260b2c6a5b1f4c920e0141177b04dfe3
Om Pathak <ompathakin194@gmail.com>
2025-01-25 21:53:21 +0530

    added start and reroll functionality

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-var x = Math.floor((Math.random() * 6) + 1);
-var y = Math.floor((Math.random() * 6) + 1);
-
-console.log(x);
-console.log(y);
-
-document.getElementsByClassName("img1")[0].src = "images/dice"+x+".png";
-
-document.getElementsByClassName("img2")[0].src = "images/dice"+y+".png";
-if(x > y){
-    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-}else if(x <y){
-
-    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
-}else{
-
-    document.querySelector("h1").innerHTML = "Its a Draw!";
+const firstLoad = () => {
+    const entryElement = document.getElementById("entry");
+    const gameElement = document.getElementById("game");
+    if (gameElement.hidden == true) {
+        entryElement.hidden = true;
+        gameElement.hidden = false;
+    }
 }
 
+const rollDice = () => {
+    var x = Math.floor((Math.random() * 6) + 1);
+    var y = Math.floor((Math.random() * 6) + 1);
+
+    if(x > y) {
+        document.getElementById("result").innerHTML = "🚩Player 1 Wins!";
+    } else if(x < y) {
+
+        document.getElementById("result").innerHTML = "Player 2 Wins!🚩";
+    } else {
+
+        document.getElementById("result").innerHTML = "Its a Draw!";
+    }
+
+    document.getElementById("dice-1").src = "images/dice"+x+".png";
+    document.getElementById("dice-2").src = "images/dice"+y+".png";
+}
+
+document.getElementById("start-btn").addEventListener("click", () => {
+    firstLoad();
+    rollDice();
+})
+
+document.getElementById("reroll-btn").addEventListener("click", () => {
+    rollDice();
+})
